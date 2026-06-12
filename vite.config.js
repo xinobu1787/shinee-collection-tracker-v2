8ce11f43031df1f6c16d1f3fcbdc9c2ed5ce9aea
tomoko.T <tamuko.0107.2jin.1118.1214@gmail.com>
2026-06-13 00:59:44 +0900

    deploy test7

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,6 @@ export default defineConfig({
     },
     build: {
         outDir: 'public/build', // ここが明示されていないと、Laravelが探す場所とズレる
-        manifest: true,
+        manifest: 'manifest.json',
     },
 });
